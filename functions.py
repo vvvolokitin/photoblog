f4122cb92e2981942768bd45ee6d465433141f7b
Nikita Volokitin <n.a.volokitin@gmail.com>
2023-12-13 16:54:49 +0500

    Create a function load_posts

--- functions.py
+++ functions.py
@@ -1,1 +1,16 @@
 import json
+
+
+POST_PATH = 'posts.json'
+
+
+def load_posts() -> list[dict[str, str]]:
+    """
+    Возвращает список постов.
+
+    Возвращаемое значение:
+        posts_list: list[dict[str, str]]: Информация о постах.
+    """
+    with open(POST_PATH, 'r', encoding='utf-8') as file:
+        posts_list: list[dict[str, str]] = json.load(file)
+    return posts_list
